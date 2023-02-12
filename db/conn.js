e382af167afa91e48a2f15b54472c245356716ac
HRIDAYESH PADALKAR <hridayeshpadalkar7@gmail.com>
2023-02-12 11:57:56 +0530

    Jo Jo Jotaro

--- db/conn.js
+++ db/conn.js
@@ -1,15 +1,15 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-const db=process.env.CONN_STRING;
-const connectDb = async()=>{
-    try{
+const db = process.env.CONN_STRING;
+const connectDb = async () => {
+    try {
         const conn = await mongoose.connect(db);
         console.log(`MongoDb connected ${conn.connection.host}`);
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         process.exit(1);
     }
 }
-module.exports={connectDb};
+module.exports = { connectDb, db };
